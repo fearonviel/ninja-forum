0a8611a768c52c7536275700b838631d38b40906
Anja <anja_bukovec@t-2.net>
2017-10-01 17:56:59 +0200

    add new topic

--- handlers/base.py
+++ handlers/base.py
@@ -1,6 +1,7 @@
 import webapp2
 import os
 import jinja2
+from google.appengine.api import users
 
 template_dir = os.path.join(os.path.dirname(__file__), "../templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
@@ -22,9 +23,19 @@ class BaseHandler(webapp2.RequestHandler):
         if not params:
             params = {}
 
+        #piskotek
         piskotek = self.request.cookies.get("piskotek")
         if piskotek:
             params["piskotek"] = True
+
+        #google login
+        user = users.get_current_user()
+
+        if user:
+            params["user"] = user
+            params["logout_url"] = users.create_logout_url("/")
+        else:
+            params["login_url"] = users.create_login_url("/")
 
         template = jinja_env.get_template(view_filename)
         return self.response.out.write(template.render(params))
@@ -32,6 +43,7 @@ class BaseHandler(webapp2.RequestHandler):
 
 class MainHandler(BaseHandler):
     def get(self):
+
         return self.render_template("home.html")
 
 
@@ -39,4 +51,5 @@ class CookieHandler(BaseHandler):
     def post(self):
         self.response.set_cookie(key="piskotek", value="nastavljen")
         return self.redirect('/')
+
 
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import webapp2
 from handlers.base import MainHandler, CookieHandler
+from handlers.topics import TopicAddHandler
 
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler, name="main-page"),
     webapp2.Route('/set-cookie', CookieHandler),
+    webapp2.Route('/topic/add', TopicAddHandler, name="topic-add"),
 ], debug=True)
