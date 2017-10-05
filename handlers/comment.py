6b87da53c64185b02cfaf4f15a3dfe620f14ff83
Anja <anja_bukovec@t-2.net>
2017-10-05 17:48:04 +0200

    added function create_comment

--- handlers/comment.py
+++ handlers/comment.py
@@ -9,13 +9,11 @@ from utils.decorators import validate_csrf
 class CommentHandler(BaseHandler):
     @validate_csrf
     def post(self, topic_id):
-        user = users.get_current_user()
-        topic = Topic.get_by_id(int(topic_id))
 
         content = cgi.escape(self.request.get("content"))
+        topic = Topic.get_by_id(int(topic_id))
 
-        new_comment = Comment(content=content, user_email=user.email(), topic_id=topic.key.id(), topic_title=topic.title)
-        new_comment.put()
+        Comment.create_comment(topic_id, content)
 
         return self.redirect_to("topic-details", topic_id=topic.key.id())
 
--- models/comment.py
+++ models/comment.py
@@ -1,4 +1,7 @@
 from google.appengine.ext import ndb
+from google.appengine.api import users
+
+from models.topic import Topic
 
 
 class Comment(ndb.Model):
@@ -9,3 +12,14 @@ class Comment(ndb.Model):
     created_at = ndb.DateTimeProperty(auto_now_add=True)
     updated_at = ndb.DateTimeProperty(auto_now=True)
     deleted = ndb.BooleanProperty(default=False)
+
+    @staticmethod
+    def create_comment(topic_id, content):
+        user = users.get_current_user()
+        topic = Topic.get_by_id(int(topic_id))
+
+        new_comment = Comment(content=content, user_email=user.email(), topic_id=topic.key.id(),
+                              topic_title=topic.title)
+        new_comment.put()
+
+#comment = Comment.create_comment() - na tak nacin se klice funkcija ki je znotraj class, Comment damo brez oklepajev.
